a02a8c792224e0b4859c7678ec8b633e3274fc4f
Vishnu <vishkrish200@gmail.com>
2024-04-05 16:37:56 +0530

    added ui changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [orderId, setOrderId] = useState('');
+  const [shipmentData, setShipmentData] = useState(null);
+
+  async function fetchData() {
+    try {
+      const response = await fetch(`http://localhost:3000/getDHL`);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      setShipmentData(data);
+    } catch (error) {
+      console.error('Fetch error:', error);
+    }
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Shipment Tracker</h1>
+      <input
+        type="text"
+        value={orderId}
+        onChange={(e) => setOrderId(e.target.value)}
+        placeholder="Enter Order ID"
+      />
+      <button onClick={fetchData}>Track Shipment</button>
+      {shipmentData && (
+        shipmentData.shipments.map((shipment, index) => (
+          <div key={index}>
+            <h2>Shipment ID: {shipment.id}</h2>
+            <p>Service: {shipment.service}</p>
+            <p>Origin: {shipment.origin.address.addressLocality}</p>
+            <p>Destination: {shipment.destination.address.addressLocality}</p>
+            <p>Status: {shipment.status.status} at {shipment.status.timestamp}</p>
+            <div>
+              <h3>Events:</h3>
+              {shipment.events.map((event, index) => (
+                <div key={index}>
+                  <p>{event.timestamp} - {event.description}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))
+      )}
     </div>
   );
 }
